115e8d7118fbdd6edc7422209d659b9b8b807960
romvano <romvano@gmail.com>
2016-09-30 20:03:29 +0300

    eslinted loginView

--- public/js/views/loginView.js
+++ public/js/views/loginView.js
@@ -1,21 +1,17 @@
-(function() {
-    'use strict';
-
-    function showLogin () {
-        if (typeof window === 'object') {
-            let LoginForm = window.LoginForm;
-            document.body.classList.add('body-login');
-//        document.querySelector('.main__login').style.display = 'block';
-            console.log('showLogin');
-            let loginForm = new LoginForm();
-        }
+(function () {
+  function showLogin() {
+    if (typeof window === 'object') {
+      const LoginForm = window.LoginForm;
+      document.body.classList.add('body-login');
+      const loginForm = new LoginForm();
     }
+  }
 
-    function hideLogin () {
-        document.body.classList.remove('body-login');
-        document.querySelector('.main__login').remove();
-    }
+  function hideLogin() {
+    document.body.classList.remove('body-login');
+    document.querySelector('.main__login').remove();
+  }
 
-    window.showLogin = showLogin;
-    window.hideLogin = hideLogin;
-}) ();
+  window.showLogin = showLogin;
+  window.hideLogin = hideLogin;
+}());
