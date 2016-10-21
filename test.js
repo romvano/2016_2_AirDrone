3addcb4a298dfbed221d141cd7a126c16a8f8bec
romvano <romvano@gmail.com>
2016-10-21 13:59:43 +0300

    what to do with tests?

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-//let assert = require('assert');
-//let hello = require('./public/main').hello;
-//let filter = require('./public/main').filter;
-//
-//
-//
-//assert.equal(hello('Test'), 'Привет, Test');
-//TODO: Кейсы для функции filter
-// assert.equal(filter('КЕК'), '***');
+global.window = global;
+
+let Component = require('./public/js/models/components/Component');
+let Button = require('./public/js/models/components/Button');
+let Header = require('./public/js/models/components/Header');
+let Link = require('./public/js/models/components/Link');
+let Message = require('./public/js/models/components/Message');
+let Input = require('./public/js/models/components/Input');
+let LoginForm = require('./public/js/models/LoginForm');
+
+//let lf = new window.LoginForm();
