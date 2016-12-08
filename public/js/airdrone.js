f327a2fd22c5b523a40f9e8e9b1f85d656e58a8a
romvano <romvano@gmail.com>
2016-12-08 21:24:51 +0300

    scoreboard promise

--- public/js/airdrone.js
+++ public/js/airdrone.js
@@ -1,10 +1,12 @@
 import Router from './modules/router';
 import LoginView from './views/loginView';
 import RegistrationView from './views/registrationView';
+import ScoreboardView from './views/scoreboardView';
 
 let router = new Router();
-router.addRoute('/login', LoginView)
-router.addRoute('/registration', RegistrationView)
+router.addRoute('/login', LoginView);
+router.addRoute('/registration', RegistrationView);
+router.addRoute('/scoreboard', ScoreboardView);
 router.addRoute('/', LoginView);
 router.start();
 window.router = router;
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ let technolibs = require('technolibs');
 
 app.use('/', express.static('public', { maxAge: 1 }));
 app.use('/chat', express.static('public', {maxAge: 1}));
+app.use('/login', express.static('public', {maxAge: 1}));
+app.use('/registration', express.static('public', {maxAge: 1}));
+app.use('/scoreboard', express.static('public', {maxAge: 1}));
 technoDoc.generate(require('./api'), 'public');
 
 app.use(parser.json());
@@ -42,6 +45,10 @@ app.post('/user', (req, res) => {
 app.post('/session', (req, res) => {
          res.send(String('Добро пожаловать, пользователь!'));
          });
+
+app.get('/rating', (req, res) => {
+    res.send(JSON.stringify([{username: 'abc1', email: 'q@q.qqqqqqq', score: 43, games: 3 }, {username: 'abs2', email: 'w@wwwwwwwww.www', score: 23, games: 553}]));
+})
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`App started on port ${process.env.PORT || 3000}`);
