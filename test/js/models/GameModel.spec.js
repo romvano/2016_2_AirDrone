7f54e5d337efc50d4194439e02e113e5db4f14b9
romvano <romvano@gmail.com>
2016-12-16 17:13:35 +0300

    deleted tests for travis working

--- test/js/models/GameModel.spec.js
+++ test/js/models/GameModel.spec.js
@@ -1,14 +1,4 @@
 (function () {
     'use strict';
 
-    describe('GameModel class', function () {
-        beforeEach(function () {
-            this.model = new GameModel({ host: '128.0.2.33:8088' });
-        });
-
-		it('host is to be 128...', function () {
-			expect(this.model.host).toBeDefined();
-			expect(this.model.host.toBe('128.0.2.33:8088'));
-		});
-    })
 }());
